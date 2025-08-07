6389ded352e46936e5871c6776201252a12dba70
lyrapp <hello.lyr.app@gmail.com>
2025-08-06 23:56:10 -0700

    update string

--- js/video.js
+++ js/video.js
@@ -201,7 +201,7 @@ createContinuousScrollText: function(htmlStory, templateData) {
         
  // Join sentences with better spacing and add end indicator
         this.scrollText = processedSentences.join(' &nbsp;&nbsp; ') + 
-            ' &nbsp;&nbsp;&nbsp;&nbsp; <span style="color: #00BBF9; font-weight: 900; font-size: 1.2em; text-shadow: 3px 3px 6px rgba(0,0,0,0.9);">🎬 THE END 🎬</span>';
+            ' &nbsp;&nbsp;&nbsp;&nbsp; <span style="color: #00BBF9; font-weight: 900; font-size: 1.2em; text-shadow: 3px 3px 6px rgba(0,0,0,0.9);">👏 THE END 👏</span>';
         console.log('Created continuous scroll text with sentence spacing, filled word highlighting, and end indicator');
 
         // Create caption overlay container
